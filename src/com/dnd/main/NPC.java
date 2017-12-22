d025ba93aad9885a6b03c15e63fedd6823ac3c0f
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2017-12-22 02:18:07 -0500

    Hieght generation with broken algorythm

--- src/com/dnd/main/NPC.java
+++ src/com/dnd/main/NPC.java
@@ -8,21 +8,28 @@ public class NPC {
 	private Gender gender;
 	private Race race;
 	private int age;
+	private int height;
+	private int weight;
+	private String name;
+	private int gold;
 	
 	public NPC() {
 		this.gender =  Gender.randomGender(); // Gives the NPC a random gender
 		this.race = new Race();
 		this.age = randAge();
+		this.height =  randHeight();
 	}
 	
+
 	// Print out all qualities of the NPC
 	public String npcDescription() {
 		String description = "";
 		
 		
 		description +=  "Race: " + this.race.getRace() + "\n";
-		description +=  "Age: " + this.age + "\n";
 		description +=  "Gender: " + this.gender + "\n";
+		description +=  "Age: " + this.age + "\n";
+		description +=  "Height: " + this.height + " cm\n";
 		
 		
 		return description;
@@ -31,6 +38,27 @@ public class NPC {
 	public int randAge() {
 		double r =  Math.random() + 0.1;
 		return (int)( r * Integer.parseInt(Race.raceList[race.getRaceIndex()][1]) );
+	}
+	
+	private int randHeight() {
+		int h = 0;
+		double a = 1;
+		double gen = 1;
+		
+		int maxA = Integer.parseInt(Race.raceList[race.getRaceIndex()][1])/4;
+		int avHieght = Integer.parseInt(Race.raceList[race.getRaceIndex()][2]);
+		
+		if(this.age < maxA) {
+			a = age/(maxA) + 0.2;
+		} 
+		if(gender == Gender.Female) {
+			gen = 0.8;
+		}
+		
+		h = avHieght/2 + (int)(avHieght*a*gen*(Math.random() ));
+		
+		return h;
+		
 	}
 	
 }
